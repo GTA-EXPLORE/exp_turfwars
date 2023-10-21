94e2be88736567c3c73b8cf545fd54aade9ebb9e
MilyonJames <clement.dolo@gmail.com>
2023-10-21 12:25:47 +0200

    de locale, by xionerazer

---
+++ locales/de.lua
@@ -0,0 +1,17 @@
+Locales["de"] = { -- Made By xionerazer
+    ["cop_notif"] = "Ein ~b~Drogen verkauf~s~ ist im gange.",
+    ["gang_notif"] = "Jemand verkauf in deiner Hood, ~b~%s~s~.",
+    ["empty"] = "~r~Du hast nichts zu verkaufen.~s~",
+    ["capture"] = "Diese Hood ~b~%s~s~ gehÃ¶rt nun deiner Gang.",
+    ["not_enough_cops"] = "~r~Es sind nicht genug Polizisten online.~s~",
+    ["notif_title"] = "Drogen verkauf",
+    ["sold"] = "Du hast  ~b~%s %s(s)~s~ verkauft.",
+
+    -- TURF NAMES
+    ["DEL_PERRO"] = "Del Perro",
+    ["VINEWOOD"] = "Vinewood",
+    ["MIRROR"] = "Mirror Park",
+    ["VESPUCCI"] = "Vespucci",
+    ["LITTLE_SEOUL"] = "Little Seoul",
+    ["CYPRESS_FLATS"] = "Cypress Flats"
+}
